af70d5e24f31b98f2da367196935d7937d2badd5
Nelosh <asakura.jo@gmail.com>
2015-12-12 11:52:11 +0200

    Sorry :)

--- src/main/scala-2.11/World/WorldOrder.scala
+++ src/main/scala-2.11/World/WorldOrder.scala
@@ -7,6 +7,6 @@ import ResourceClasses._
  * Created by Dmitrij on 11.12.2015.
  */
 object WorldOrder {
-  val worldResources = Map[Resource, List[Resource]] //Predef.Map() : Map[Resource, List[ResourcePack]]
+  val worldResources = Map[Resource, List[ResourcePack]] //Predef.Map() : Map[Resource, List[ResourcePack]]
 
 }
